7e2d7e439510014c14352f94b51ae7b5911212c8
Hirthuk <sharankumarp02@gmail.com>
2025-05-09 10:52:42 +0530

    AddList worked

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5173,
     historyApiFallback: true,  // Critical for client-side routing
   },
   build: {
